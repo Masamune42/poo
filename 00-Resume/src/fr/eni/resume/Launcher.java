d0c264e1017a187fb69b9fb488117f3370deb2e3
Masamune42 <a.caignard@hotmail.fr>
2020-02-25 09:02:30 +0100

    Ajout révision : méthodes utiles + lecture de fichier

--- 00-Resume/src/fr/eni/resume/Launcher.java
+++ 00-Resume/src/fr/eni/resume/Launcher.java
@@ -1,5 +1,9 @@
 package fr.eni.resume;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -42,6 +46,101 @@ public class Launcher {
 		System.out.println("4-Collections");
 		collections();
 		System.out.println("---------------------------------------");
+
+		// Fonctions utiles
+		System.out.println("5-Fonctions utiles");
+		fonctionsUtiles();
+		System.out.println("---------------------------------------");
+
+		// Lecture/Ecriture de fichiers
+		System.out.println("6-Files");
+		files();
+		System.out.println("---------------------------------------");
+	}
+
+
+	private static void files() {
+		List<String> listeMots = new ArrayList<>();
+		try {
+			// On spécifie l'adresse locale du dico
+			File addr = new File("C:\\Users\\acaignar2019\\Documents\\06-POO\\TP\\dictionnaire.txt");
+			// On lit et on stock tous les mots dans le buffer dico
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(addr), "UTF8"));
+			String str;
+			
+
+			// Tant qu'il y a des mots dans le buffer, on les ajoute à la liste
+			while ((str = bfr.readLine()) != null) {
+				// System.out.println(str);
+				listeMots.add(str);
+			}
+			
+
+			bfr.close();
+		} catch (Exception e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		
+		// System.out.println(listeMots);
+		
+	}
+
+	private static void fonctionsUtiles() {
+		List<String> liste = new ArrayList<>();
+		liste.add("bob");
+		liste.add("john");
+		liste.add("ted");
+		liste.add("garry");
+		String string = "Test sur une chaine de caractères";
+		String[] tab = { "chat", "chien", "corbeau", "renard", "loup" };
+		Animal chat1 = new Chat("kiki", 19, new Humain(), 0);
+
+		// String
+		System.out.println("String ->");
+
+		System.out.println("toUpperCase : " + string.toUpperCase());
+		System.out.println("toLowerCase : " + string.toLowerCase());
+		System.out.println("length : " + string.length());
+
+		// String -> tableaux de char
+		System.out.println("toCharArray : ");
+		for (char c : string.toCharArray()) {
+			System.out.print(c + " ");
+		}
+		System.out.println();
+
+		// tableau -> String
+		String tabToString = String.join(", ", tab);
+		System.out.println("Tableau en String : " + tabToString);
+
+		// Tableaux
+		System.out.println("Tableaux ->");
+		// sort
+		Arrays.sort(tab);
+		System.out.print("sort tableau : ");
+		for (String string2 : tab) {
+			System.out.print(string2 + " ");
+		}
+		System.out.println();
+
+		// Collections
+		System.out.println("Collectons ->");
+		System.out.println("Liste : " + liste);
+
+		// shuffle
+		Collections.shuffle(liste);
+		System.out.println("Liste mélangée : " + liste);
+
+		// Objet
+		System.out.println("Objet ->");
+		boolean b1 = chat1 instanceof Chat;
+		boolean b2 = chat1 instanceof Animal;
+		boolean b3 = chat1 instanceof Chien;
+		System.out.println("chat instanceof Chat: " + b1);
+		System.out.println("chat instanceof Animal : " + b2);
+		System.out.println("chat instanceof  Chien: " + b3);
+
 	}
 
 	private static void collections() {
@@ -142,25 +241,25 @@ public class Launcher {
 		humain.setPrenom("Alex");
 		humain.setNom("C");
 		humain.setAge(42);
-		
+
 		Animal chat = new Chat("kiki", 20, humain, 0);
 		Animal chat1 = new Chat("snowki", 8, humain, 15);
 		Animal chat2 = new Chat("molly", 12, humain, 10);
 		Animal chat3 = new Chat("fripouille", 14, humain, 5);
-		
+
 		List<Animal> animaux = new ArrayList<>();
 		animaux.add(chat);
 		animaux.add(chat1);
 		animaux.add(chat2);
 		animaux.add(chat3);
-		
+
 		Collections.sort(animaux, new SortAgeComparator());
 		System.out.println("tri de la liste d'animaux par age");
 		Iterator<Animal> itAnimal = animaux.iterator();
-		
+
 		while (itAnimal.hasNext()) {
 			Animal animal = itAnimal.next();
-			System.out.println(animal.getNom() +" : "+animal.getAge());
+			System.out.println(animal.getNom() + " : " + animal.getAge());
 		}
 
 		// méthodes de Collections
@@ -168,7 +267,7 @@ public class Launcher {
 		System.out.println("arrayList après sort :");
 		System.out.print(arrayList);
 		System.out.println();
-		
+
 		Collections.reverse(arrayList);
 		System.out.println("arrayList après reverse :");
 		System.out.print(arrayList);
